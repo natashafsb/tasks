638e0c40e8c4ad31d1a8bf931ac5c49c51cfede8
natashafsb <skvernenko@mail.ru>
2023-02-25 19:51:36 +0300

    made task 64

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string t = Console.ReadLine();
+int n = Convert.ToInt32(t);
+t = Console.ReadLine();
+int m = Convert.ToInt32(t);
+for (int i = n; i <= m; i++)
+{
+    Console.Write(i);
+    Console.Write(' ');
+}
